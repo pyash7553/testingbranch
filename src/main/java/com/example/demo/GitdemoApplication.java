02004e9a26073f7ce4f3d9bc0bbc897861d0c41c
pyash7553 <pyash7553@gmail.com>
2022-07-14 17:09:24 +0530

    first

---
+++ src/main/java/com/example/demo/GitdemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitdemoApplication.class, args);
+	}
+	
+	public void f() {
+		System.out.println("Dev...");
+	}
+
+}
